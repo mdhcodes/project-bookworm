08c8b0512336304ad39e9556309a5f26c3e451a3
Marina Bain <mjbain10@gmail.com>
2017-03-22 18:39:09 -0400

    In progress results CSS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,193 +1,255 @@
-// Google Books API - Mood and Genre Search
+// Stored function for Google Books API HTML write
+function resultsGoogle (obj) {
+    // For loop - write results to DOM, generates modal buttons
+    for (var i = 0; i < obj.items.length; i++) {
+        var title = obj.items[i].volumeInfo.title;
+        // var author = obj.items[i].volumeInfo.authors[0];
+        var image = obj.items[i].volumeInfo.imageLinks.thumbnail;
+        var description = obj.items[i].volumeInfo.description;
+        var isbn = obj.items[i].volumeInfo.industryIdentifiers[0].identifier;
+       
+        // Test Modal
+        var modalBtn = $('<button/>', {
+            id: 'btn-modal-' + i,
+            class: 'modal-btn btn-floating waves-effect waves-light',
+            'data-isbn': isbn
+        });
+        var modalIcon = $('<i/>', {
+            class: 'material-icons',
+            text: 'add'
+        });
+        $(modalBtn).append(modalIcon);
+        // End test modal
+        
+        $('.api-data').append('<!--   Icon Section   -->');
+        var col = $('<div/>', {
+            class: 'col s12 m6 l3'
+        });
+        $('.api-data').append(col);
+        var iconBlock = $('<div/>', {
+            class: 'icon-block'  
+        });
+        col.append(iconBlock);
+        var card = $('<div/>', {
+            class: 'card medium'
+        });
+        iconBlock.append(card);
+        var cardImage = $('<div/>', {
+            class: 'card-image waves-effect waves-block waves-light'
+        });
+        card.append(cardImage);
+        var imageDisp = $('<img/>', {
+            class: 'activator',
+            src: image
+        });
+        cardImage.append(imageDisp);
+        var cardContent = $('<div/>', {
+            class: 'card-content'
+        });
+        card.append(cardContent);
+        var cardTitle = $('<span/>', {
+            class: 'card-title activator grey-text text-darken-4',
+            text: title
+        });
+        cardContent.append(cardTitle);
+        
+        cardContent.append(modalBtn);
+        // cardContent.append(modalBtn);
+    } //End for loop
 
-$('.search').on('click', function() {
-    $('#results-div').html("");
-    var search = $(this).attr("id");
-    console.log(search);
-
-    var googleBooksURL = 'https://www.googleapis.com/books/v1/volumes?q=' + search + '&maxResults=8';
-
-    var h3 = $('<h3 class="list-name">');
-    h3.text($(this).text());
-
-    $.ajax({
-        url: googleBooksURL,
-        method: 'GET',
-        dataType: 'jsonp'
-    }).done(function(result) {
-        console.log('Google Books', result);
-
-        $('#results-div').append(h3);
-        $('#results-div').append('<div class="api-data">');
-        setTimeout(function() {
-            $('#results-collapse').trigger('click');
-        }, 200);
-        for (var i = 0; i < result.items.length; i++) {
-            console.log(result.items[i].volumeInfo.imageLinks.thumbnail)
-
-            $('.api-data').append('<!--   Icon Section   -->');
-            var col = $('<div class="col s12 m6 l3">');
-            $('.api-data').append(col);
-            var iconBlock = $('<div class="icon-block">');
-            col.append(iconBlock);
-            var card = $('<div class="card medium">');
-            iconBlock.append(card);
-            var cardImage = $('<div class="card-image waves-effect waves-block waves-light">');
-            card.append(cardImage);
-            var image = $('<img class="activator" src="' + result.items[i].volumeInfo.imageLinks.thumbnail + '">');
-            cardImage.append(image);
-            var cardContent = $('<div class="card-content">');
-            card.append(cardContent);
-            var cardTitle = $('<span class="card-title activator grey-text text-darken-4">' + result.items[i].volumeInfo.title + '<i class="material-icons right">more_vert</i></span>');
-            cardContent.append(cardTitle);
-            var link = $('<p><a href="#">This is a link</a></p>');
-            cardContent.append(link);
-            var cardReveal = $('<div class="card-reveal">');
-            card.append(cardReveal);
-            var cardRevealTitle = $('<span class="card-title grey-text text-darken-4">' + result.items[i].volumeInfo.title + '<br>' + result.items[i].volumeInfo.authors + '<i class="material-icons right">close</i></span>');
-            cardReveal.append(cardRevealTitle);
-            var description = $('<p>' + result.items[i].volumeInfo.description + '</p>');
-            cardReveal.append(description);
-
-        }
-    }).fail(function(error) {
-        console.log('Google Books: An error occurred.');
-    });
-
-});
+} //End resultsGoogle
 
 
-// Google Books API - Author Search
 
-$("#author-search").on('click', function() {
 
-    $('#results-div').html("");
 
-    //Grab the text from the user
-    var author = $("#author").val().trim();
 
-    var googleBooksURL = 'https://www.googleapis.com/books/v1/volumes?q=' + author + '&maxResults=8';
+$(document).ready(function() {
 
-    $.ajax({
-        url: googleBooksURL,
-        method: 'GET',
-        dataType: 'jsonp'
-    }).done(function(result) {
-        console.log('Google Books', result);
+    
+    
+// Mood Search - Google Books API
+    $('.search').on('click', function() {
+        // Reset results div
+        $('#results-div').html("");
+        
+        // Title for results - storing
         var h3 = $('<h3 class="list-name">');
-        h3.text(author);
-        $('#results-div').append(h3);
-        $('#results-div').append('<div class="api-data">');
-        setTimeout(function() {
-            $('#results-collapse').trigger('click');
-        }, 200);
-        for (var i = 0; i < result.items.length; i++) {
+        h3.text($(this).text());
+        // Set search term
+        var search = $(this).attr("id");
+        console.log(search);
+        var googleBooksURL = 'https://www.googleapis.com/books/v1/volumes?q=' + search + '&filter=partial&printType=books&maxResults=8';
+        // API Call
+        $.ajax({
+            url: googleBooksURL,
+            method: 'GET',
+            dataType: 'jsonp'
+        }).done(function(result) {
+            // Expand results element
+            setTimeout(function() {
+                $('#results-collapse').trigger('click');
+            }, 100);
+            setTimeout(function() {
+                $('#apiData').fadeTo(500, 1);
+            }, 200);
+            // Console log JSON results
+            console.log('Google Books', result);
+            // Title for results - writing to DOM
+            $('#results-div').append(h3);
+            $('#results-div').append('<div class="api-data">');
+            // Call function to write HTML
+            resultsGoogle(result);
+        }).fail(function(error) {
+            console.log('Google Books: An error occurred.');
+        });
 
-            $('.api-data').append('<!--   Icon Section   -->');
-            var col = $('<div class="col s12 m6 l3">');
-            $('.api-data').append(col);
-            var iconBlock = $('<div class="icon-block">');
-            col.append(iconBlock);
-            var card = $('<div class="card">');
-            iconBlock.append(card);
-            var cardImage = $('<div class="card-image waves-effect waves-block waves-light">');
-            card.append(cardImage);
-            var image = $('<img class="activator" src="' + result.items[i].volumeInfo.imageLinks.thumbnail + '">');
-            cardImage.append(image);
-            var cardContent = $('<div class="card-content">');
-            card.append(cardContent);
-            var cardTitle = $('<span class="card-title activator grey-text text-darken-4">' + result.items[i].volumeInfo.title + '<i class="material-icons right">more_vert</i></span>');
-            cardContent.append(cardTitle);
-            var link = $('<p><a href="#">This is a link</a></p>');
-            cardContent.append(link);
-            var cardReveal = $('<div class="card-reveal">');
-            card.append(cardReveal);
-            var cardRevealTitle = $('<span class="card-title grey-text text-darken-4">' + result.items[i].volumeInfo.title + '<br>' + result.items[i].volumeInfo.authors + '<i class="material-icons right">close</i></span>');
-            cardReveal.append(cardRevealTitle);
-            var description = $('<p>' + result.items[i].volumeInfo.description + '</p>');
-            cardReveal.append(description);
+    }); //End Mood Search
 
-        }
 
-    }).fail(function(error) {
-        console.log('Google Books: An error occurred.');
+// Author Search - Google Books API
+    $("#author-search").on('click', function() {
+        // Clear results HTML
+        $('#results-div').html("");
+
+        //Grab the text from the user input
+        var author = $("#author").val().trim();
+
+        var googleBooksURL = 'https://www.googleapis.com/books/v1/volumes?q=' + author + '&filter=partial&printType=books&maxResults=8';
+        // API call
+        $.ajax({
+            url: googleBooksURL,
+            method: 'GET',
+            dataType: 'jsonp'
+        }).done(function(result) {
+            console.log('Google Books', result);
+            // Expand results element
+            setTimeout(function() {
+                $('#results-collapse').trigger('click');
+            }, 100);
+            // Write results to DOM
+            var h3 = $('<h3 class="list-name">');
+            h3.text(author);
+            $('#results-div').append(h3);
+            $('#results-div').append('<div class="api-data">');
+            for (var i = 0; i < result.items.length; i++) {
+
+                $('.api-data').append('<!--   Icon Section   -->');
+                var col = $('<div class="col s12 m6 l3">');
+                $('.api-data').append(col);
+                var iconBlock = $('<div class="icon-block">');
+                col.append(iconBlock);
+                var card = $('<div class="card">');
+                iconBlock.append(card);
+                var cardImage = $('<div class="card-image waves-effect waves-block waves-light">');
+                card.append(cardImage);
+                var image = $('<img class="activator" src="' + result.items[i].volumeInfo.imageLinks.thumbnail + '">');
+                cardImage.append(image);
+                var cardContent = $('<div class="card-content">');
+                card.append(cardContent);
+                var cardTitle = $('<span class="card-title activator grey-text text-darken-4">' + result.items[i].volumeInfo.title + '<i class="material-icons right">more_vert</i></span>');
+                cardContent.append(cardTitle);
+                var link = $('<p><a href="#">This is a link</a></p>');
+                cardContent.append(link);
+                var cardReveal = $('<div class="card-reveal">');
+                card.append(cardReveal);
+                var cardRevealTitle = $('<span class="card-title grey-text text-darken-4">' + result.items[i].volumeInfo.title + '<br>' + result.items[i].volumeInfo.authors + '<i class="material-icons right">close</i></span>');
+                cardReveal.append(cardRevealTitle);
+                var description = $('<p>' + result.items[i].volumeInfo.description + '</p>');
+                cardReveal.append(description);
+
+            }
+
+        }).fail(function(error) {
+            console.log('Google Books: An error occurred.');
+        });
+
+    }); //End author search
+
+
+// Best Seller Search - NYT API
+    $('#nytButton').on('click', function() {
+        // Clear results HTML
+        $('#results-div').html("");
+
+        var nyTimesKey = 'a9c6282043404e258f246983bccaf593';
+        var nyTimesURL = 'https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=' + nyTimesKey;
+        // API call
+        $.ajax({
+            url: nyTimesURL,
+            method: 'GET'
+        }).done(function(result) {
+            console.log('NY Times', result);
+            // Expand results element by triggering click
+            setTimeout(function() {
+                $('#results-collapse').trigger('click');
+            }, 200);
+            // Write results to DOM
+            var h3 = $('<h3>');
+            h3.text(result.results.lists[0].display_name);
+            $('#results-div').append(h3);
+            $('#results-div').append('<div class="api-data">');
+            for (var i = 0; i < result.results.lists.length; i++) {
+
+                $('.api-data').append('<!--   Icon Section   -->');
+                var col = $('<div class="col s12 m6 l3">');
+                $('.api-data').append(col);
+                var iconBlock = $('<div class="icon-block">');
+                col.append(iconBlock);
+                var card = $('<div class="card">');
+                iconBlock.append(card);
+                var cardImage = $('<div class="card-image waves-effect waves-block waves-light">');
+                card.append(cardImage);
+                var image = $('<img class="activator" src="' + result.results.lists[0].books[i].book_image + '">');
+                cardImage.append(image);
+                var cardContent = $('<div class="card-content">');
+                card.append(cardContent);
+                var cardTitle = $('<span class="card-title activator grey-text text-darken-4">' + result.results.lists[0].books[i].title + '<i class="material-icons right">more_vert</i></span>');
+                cardContent.append(cardTitle);
+                //var link = $('<p><a href="#">This is a link</a></p>');
+                //var link = $('<p><a id="' + i + '" href="' + result["ISBN:" + result.results.lists[0].books[i].primary_isbn10 + ""].url + '" class="find-book" data-isbn="' + result.results.lists[0].books[i].primary_isbn10 + '">Find this book</a></p>');
+                var link = $('<p><a id="' + i + '" class="find-book" data-isbn="' + result.results.lists[0].books[i].primary_isbn10 + '">Find this book</a></p>');
+                cardContent.append(link);
+                var cardReveal = $('<div class="card-reveal">');
+                card.append(cardReveal);
+                var cardRevealTitle = $('<span class="card-title grey-text text-darken-4">' + result.results.lists[0].books[i].title + '<br>' + result.results.lists[0].books[i].author + '<i class="material-icons right">close</i></span>');
+                cardReveal.append(cardRevealTitle);
+                var description = $('<p>' + result.results.lists[0].books[i].description + '</p>');
+                cardReveal.append(description);
+
+                console.log('ISBN' + i, result.results.lists[0].books[i].primary_isbn10);
+
+            }
+
+            console.log('link', link);
+
+        }).fail(function(error) {
+            console.log('NY Times: An error occurred.');
+        });
+    }); // end NYT Best Sellers search
+
+
+// Collapsible Behavior (results/search)
+    $('#search-collapse').on('click', function() {
+        // When search collapsable is expanded, hide the search icon and show the results icon
+        $('#search-collapse').fadeTo(100, 0);
+        $('#results-collapse').fadeTo(100, 1);
+    });
+    $('#results-collapse').on('click', function() {
+        // When results collapsible is exapnded, hide the results icon and show the search icon
+        $('#results-collapse').fadeTo(100, 0);
+        $('#search-collapse').fadeTo(100, 1);
+    });
+    
+
+// Modal Logic (results - more info display)
+    $('.modal').modal();
+    $('#apiData').on('click', '.modal-btn', function() {
+        console.log('Modal button clicked!');
     });
 
-});
 
-// NY Times Books API
-
-$('#nytButton').on('click', function() {
-
-    $('#results-div').html("");
-
-    var nyTimesKey = 'a9c6282043404e258f246983bccaf593';
-    var nyTimesURL = 'https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=' + nyTimesKey;
-    $.ajax({
-        url: nyTimesURL,
-        method: 'GET'
-    }).done(function(result) {
-        console.log('NY Times', result);
-
-        var h3 = $('<h3>');
-        h3.text(result.results.lists[0].display_name);
-        $('#results-div').append(h3);
-        $('#results-div').append('<div class="api-data">');
-        setTimeout(function() {
-            $('#results-collapse').trigger('click');
-        }, 200);
-        for (var i = 0; i < result.results.lists.length; i++) {
-
-            $('.api-data').append('<!--   Icon Section   -->');
-            var col = $('<div class="col s12 m6 l3">');
-            $('.api-data').append(col);
-            var iconBlock = $('<div class="icon-block">');
-            col.append(iconBlock);
-            var card = $('<div class="card">');
-            iconBlock.append(card);
-            var cardImage = $('<div class="card-image waves-effect waves-block waves-light">');
-            card.append(cardImage);
-            var image = $('<img class="activator" src="' + result.results.lists[0].books[i].book_image + '">');
-            cardImage.append(image);
-            var cardContent = $('<div class="card-content">');
-            card.append(cardContent);
-            var cardTitle = $('<span class="card-title activator grey-text text-darken-4">' + result.results.lists[0].books[i].title + '<i class="material-icons right">more_vert</i></span>');
-            cardContent.append(cardTitle);
-            //var link = $('<p><a href="#">This is a link</a></p>');
-            //var link = $('<p><a id="' + i + '" href="' + result["ISBN:" + result.results.lists[0].books[i].primary_isbn10 + ""].url + '" class="find-book" data-isbn="' + result.results.lists[0].books[i].primary_isbn10 + '">Find this book</a></p>');
-            var link = $('<p><a id="' + i + '" class="find-book" data-isbn="' + result.results.lists[0].books[i].primary_isbn10 + '">Find this book</a></p>');
-            cardContent.append(link);
-            var cardReveal = $('<div class="card-reveal">');
-            card.append(cardReveal);
-            var cardRevealTitle = $('<span class="card-title grey-text text-darken-4">' + result.results.lists[0].books[i].title + '<br>' + result.results.lists[0].books[i].author + '<i class="material-icons right">close</i></span>');
-            cardReveal.append(cardRevealTitle);
-            var description = $('<p>' + result.results.lists[0].books[i].description + '</p>');
-            cardReveal.append(description);
-
-            console.log('ISBN' + i, result.results.lists[0].books[i].primary_isbn10);
-
-        }
-
-        console.log('link', link);
-
-    }).fail(function(error) {
-        console.log('NY Times: An error occurred.');
-    });
-}); // end #nytButton click event
-
-
-
-// Collapsible Behavior
-$('#search-collapse').on('click', function() {
-    // $('#apiData').fadeOut('slow');
-    $('#search-collapse').fadeOut('fast');
-    $('#results-collapse').fadeIn('fast');
-});
-$('#results-collapse').on('click', function() {
-    // $('#apiData').delay('fast').fadeIn('slow');
-    $('#results-collapse').fadeOut('fast');
-    $('#search-collapse').fadeIn('fast');
-});
+}); //end DocReady
 
 
 /*
