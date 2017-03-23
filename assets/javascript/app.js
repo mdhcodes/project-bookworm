b883adcd3197ac0cfa25e9d54b6f1f9033b30d89
Michelle Hartley <mdhcodes@gmail.com>
2017-03-23 13:18:55 -0400

    Add open library link to nyt search

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,7 @@ function resultsGoogle (obj) {
     // For loop through results
     for (var i = 0; i < obj.items.length; i++) {
         var title = obj.items[i].volumeInfo.title;
-        // var author = obj.items[i].volumeInfo.authors[0]; -------------- Test with new Google shelves
+        var author = obj.items[i].volumeInfo.authors[0]; //-------------- Test with new Google shelves
         var image = obj.items[i].volumeInfo.imageLinks.thumbnail;
         var description = obj.items[i].volumeInfo.description;
         var isbn = obj.items[i].volumeInfo.industryIdentifiers[0].identifier;
@@ -68,6 +68,87 @@ function resultsGoogle (obj) {
     } //End i loop
 
 } //End resultsGoogle stored function
+
+
+// Stored function for Google Books API HTML write
+function resultsNyt (obj) {
+
+
+  // Loop through results categories
+  for(var i = 0; i < obj.results.lists.length; i++) {
+      // If statement to only select certain best-seller result categories
+      if(i === 2 || i === 3 ||  i === 4 || i === 5 || i === 11 || i === 12 || i === 13) {
+
+
+          // Write results title to DOM
+          var h3 = $('<h3>');
+          h3.text(obj.results.lists[i].display_name);
+          $('#results-div').append(h3);
+          $('#results-div').append('<div class="api-data">');
+          // Loop through results for each category
+          for(var j = 0; j < (obj.results.lists[i].books.length-1); j++) {
+              var title = obj.results.lists[i].books[j].title;
+              var author = obj.results.lists[i].books[j].author;
+              var image = obj.results.lists[i].books[j].book_image;
+              var description = obj.results.lists[i].books[j].description;
+              var isbn = obj.results.lists[i].books[j].primary_isbn10;
+
+              // Create results cards HTML
+              $('.api-data').append('<!--   Icon Section   -->');
+              var col = $('<div/>', {
+                  class: 'col s12 m6 l3'
+              });
+              $('.api-data').append(col);
+              var iconBlock = $('<div/>', {
+                  class: 'icon-block'
+              });
+              col.append(iconBlock);
+              var card = $('<div/>', {
+                  class: 'card medium'
+              });
+              iconBlock.append(card);
+              var cardImage = $('<div/>', {
+                  class: 'card-image waves-effect waves-block waves-light'
+              });
+              card.append(cardImage);
+              var imageDisp = $('<img/>', {
+                  class: 'activator',
+                  src: image
+              });
+              cardImage.append(imageDisp);
+              var cardContent = $('<div/>', {
+                  class: 'card-content'
+              });
+              card.append(cardContent);
+              var cardTitle = $('<span/>', {
+                  class: 'card-title activator grey-text text-darken-4',
+                  text: title
+              });
+              cardContent.append(cardTitle);
+
+              // Create results modal trigger button HTML
+              var modalBtn = $('<button/>', {
+                  id: 'btn-modal-' + i,
+                  class: 'modal-btn btn-floating waves-effect waves-light',
+                  'data-isbn': isbn
+              });
+              var modalIcon = $('<i/>', {
+                  class: 'material-icons',
+                  text: 'add'
+              });
+              $(modalBtn).append(modalIcon);
+              cardContent.append(modalBtn);
+
+              console.log('ISBN', obj.results.lists[i].books[j].primary_isbn10);
+
+              var libraryButton = $('<a class="waves-effect waves-light btn library" data-isbn="' + obj.results.lists[i].books[j].primary_isbn10 + '">Library Info</a>');
+              card.append(libraryButton);
+
+          } // end j for loop
+      } // end if statement
+  } // end i for loop
+
+} // end resultsNyt stored function
 
 
 $(document).ready(function() {
@@ -170,72 +251,8 @@ $(document).ready(function() {
             setTimeout(function() {
                 $('#apiData').fadeTo(500, 1);
             }, 200);
-            // Loop through results categories
-            for(var i = 0; i < result.results.lists.length; i++) {
-                // If statement to only select certain best-seller result categories
-                if(i === 2 || i === 3 || i === 4 || i === 5 || i === 9 || i === 11 || i === 12 || i === 13) {
-                    // Write results title to DOM
-                    var h3 = $('<h3>');
-                    h3.text(result.results.lists[i].display_name);
-                    $('#results-div').append(h3);
-                    $('#results-div').append('<div class="api-data">');
-                    // Loop through results for each category
-                    for(var j = 0; j < (result.results.lists[i].books.length-1); j++) {
-                        var title = result.results.lists[i].books[j].title;
-                        var author = result.results.lists[i].books[j].author;
-                        var image = result.results.lists[i].books[j].book_image;
-                        var description = result.results.lists[i].books[j].description;
-                        var isbn = result.results.lists[i].books[j].primary_isbn10;
-
-                        // Create results cards HTML
-                        $('.api-data').append('<!--   Icon Section   -->');
-                        var col = $('<div/>', {
-                            class: 'col s12 m6 l3'
-                        });
-                        $('.api-data').append(col);
-                        var iconBlock = $('<div/>', {
-                            class: 'icon-block'
-                        });
-                        col.append(iconBlock);
-                        var card = $('<div/>', {
-                            class: 'card medium'
-                        });
-                        iconBlock.append(card);
-                        var cardImage = $('<div/>', {
-                            class: 'card-image waves-effect waves-block waves-light'
-                        });
-                        card.append(cardImage);
-                        var imageDisp = $('<img/>', {
-                            class: 'activator',
-                            src: image
-                        });
-                        cardImage.append(imageDisp);
-                        var cardContent = $('<div/>', {
-                            class: 'card-content'
-                        });
-                        card.append(cardContent);
-                        var cardTitle = $('<span/>', {
-                            class: 'card-title activator grey-text text-darken-4',
-                            text: title
-                        });
-                        cardContent.append(cardTitle);
-
-                        // Create results modal trigger button HTML
-                        var modalBtn = $('<button/>', {
-                            id: 'btn-modal-' + i,
-                            class: 'modal-btn btn-floating waves-effect waves-light',
-                            'data-isbn': isbn
-                        });
-                        var modalIcon = $('<i/>', {
-                            class: 'material-icons',
-                            text: 'add'
-                        });
-                        $(modalBtn).append(modalIcon);
-                        cardContent.append(modalBtn);
-                    } // end j for loop
-                } // end if statement
-            } // end i for loop
-
+            // Execute function to write results display HTML
+            resultsNyt(result);
         }).fail(function(error) {
             console.log('NY Times: An error occurred.');
         }); // end API call
@@ -298,7 +315,7 @@ $(document).ready(function() {
 
 
 
-//Sign-In Modale
+// Sign-In Modale
     $('.modal').modal();
     $('#siBtn').on('click', function() {
         $('#signIn').modal('open');
