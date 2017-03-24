76cb2daa0a5f9f73cb672e9875dc9eca4f644f71
Marina Bain <mjbain10@gmail.com>
2017-03-24 13:41:47 -0400

    testing new sign in

--- assets/javascript/auth.js
+++ assets/javascript/auth.js
@@ -8,18 +8,5 @@ var config = {
 };    
 firebase.initializeApp(config);
 
-var uiConfig = {
-    signInSuccessUrl: 'page2.html',
-    signInOptions: [
-        // Leave the lines as is for the providers you want to offer your users.
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        firebase.auth.EmailAuthProvider.PROVIDER_ID
-    ],
-    // Terms of service url.
-    tosUrl: '<your-tos-url>'
-};
 
-// Initialize the FirebaseUI Widget using Firebase.
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
-// The start method will wait until the DOM is loaded.
-ui.start('#signIn', uiConfig);
+
