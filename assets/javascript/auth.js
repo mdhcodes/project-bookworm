f9ce96d431f3126a859eacca36f3811e1f706711
Marina Bain <mjbain10@gmail.com>
2017-03-24 15:13:44 -0400

    working auth, no style

--- assets/javascript/auth.js
+++ assets/javascript/auth.js
@@ -9,7 +9,7 @@ var config = {
 firebase.initializeApp(config);
 
 var uiConfig = {
-    signInSuccessUrl: 'page2.html',
+    signInSuccessUrl: 'index.html',
     signInOptions: [
         // Leave the lines as is for the providers you want to offer your users.
         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
@@ -25,4 +25,48 @@ ui.start('#firebaseui-auth-container', uiConfig);
 
 var database = firebase.database();
 console.log(database);
+
+// Track sign-in status
+initApp = function() {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            // User is signed in.
+            var displayName = user.displayName;
+            var email = user.email;
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
+            var uid = user.uid;
+            var providerData = user.providerData;
+            user.getToken().then(function(accessToken) {
+                var details = {
+                    displayName: displayName,
+                    email: email,
+                    emailVerified: emailVerified,
+                    photoURL: photoURL,
+                    uid: uid,
+                    accessToken: accessToken,
+                    providerData: providerData
+                };
+                console.log(details.displayName);
+                $('#sign-in-status').html('Signed in');
+                $('#sign-in').hide();
+                $('#sign-out').show();
+
+                document.getElementById('accountModal').textContent = JSON.stringify(details, null, 'null');
+            });
+        } else {
+            // User is signed out.
+            $('#sign-in-status').html('Signed out');
+            $('#sign-in').show();
+            $('#sign-out').hide();
+            $('#account-details').hide();
+        }
+    }, function(error) {
+      console.log(error);
+    });
+};
+
+window.addEventListener('load', function() {
+    initApp();
+});
 
